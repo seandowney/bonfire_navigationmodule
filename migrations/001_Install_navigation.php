c63224cd857d4ee8a9a5f8099163af76bca40b5a
Jeff Fox <jfox015@gmail.com>
2012-03-05 00:21:39 -0500

    Added custom defaulot nav entries

--- migrations/001_Install_navigation.php
+++ migrations/001_Install_navigation.php
@@ -16,11 +16,33 @@ class Migration_Install_navigation extends Migration {
 		$this->dbforge->add_key('nav_id', true);
 		$this->dbforge->create_table('navigation');
 
+        $data = array('nav_id'=>1,
+                      'title'=>'Home',
+                      'url'=>'/',
+                      'nav_group_id'=>1,
+                      'position'=>1,
+                      'parent_id'=>0,
+                      'has_kids'=>0);
+        $this->db->insert("{$prefix}navigation",$data);
+        $data = array('nav_id'=>2,
+                      'title'=>'Last Sim',
+                      'url'=>'/lastsim',
+                      'nav_group_id'=>1,
+                      'position'=>2,
+                      'parent_id'=>0,
+                      'has_kids'=>0);
+        $this->db->insert("{$prefix}navigation",$data);
+
 		$this->dbforge->add_field('`nav_group_id` int(11) NOT NULL AUTO_INCREMENT');
 		$this->dbforge->add_field('`title` VARCHAR(30) NOT NULL');
 		$this->dbforge->add_field('`abbr` VARCHAR(20) NOT NULL');
 		$this->dbforge->add_key('nav_group_id', true);
 		$this->dbforge->create_table('navigation_group');
+
+        $data = array('nav_group_id'=>1,
+            'title'=>'header_nav',
+            'abbr'=>'hn');
+        $this->db->insert("{$prefix}navigation_group",$data);
 
 		// permissions
 		// permissions
