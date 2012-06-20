05a38f9af9375a12eea7ff662c4061a1b0a8bb85
Sean Downey <sean@considerweb.com>
2012-06-20 23:04:10 +0100

    Moving the js file to assets

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,18 +1,16 @@
-$(function() {
 	$( ".sortable" ).sortable({
 		update: update_order,
 		placeholder: "ui-state-highlight"
 	}).disableSelection();
-});
 
 function update_order(event, ui) {
 	order = new Array();
 	$('tr', this).each(function(){
-		order.push( $(this).find('input[name="action_to[]"]').val() );
+		order.push( $(this).find('input[name="checked[]"]').val() );
 	});
 	order = order.join(',');
 
-	$.post('<?php echo site_url(SITE_AREA . '/content/navigation/ajax_update_positions');?>', { order: order }, function() {
+	$.post('/admin/content/navigation/ajax_update_positions', { order: order }, function() {
 		$('tr').removeClass('alt');
 		$('tr:even').addClass('alt');
 	});
